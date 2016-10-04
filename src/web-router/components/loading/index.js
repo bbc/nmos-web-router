c054992207f0b8f38b24711eb37505747d97a2ae
Luke Preston <luke.preston@bbc.co.uk>
2016-10-04 15:51:10 +0100

    gelerising loading

--- src/web-router/components/loading/index.js
+++ src/web-router/components/loading/index.js
@@ -1,56 +1,43 @@
 import './loading.css'
 
 import React, { PropTypes } from 'react'
-import { Loading, No, Yes } from '../../../gel-react/iconography'
-import Button from '../../../components/button-component'
+import { Loading as Spinner, No, Yes } from '../../../gel-react/iconography'
+import { Layout, LayoutItem } from '../../../gel-react/grid'
+import { Trafalgar, GreatPrimer, Pica } from '../../../gel-react/typography'
+import Status from './status-component'
 
-let LoadingComponent = ({view}) => {
-  return <div className={view.status}>
-    <div className='header gel-layout'>
-      <div className='gel-layout__item'>
-        <h4 className='title title-loading gel-canon'><Loading /> Loading</h4>
-        <h4 className='title title-error gel-canon'><No /> Error: Exceeded retry limit</h4>
-      </div>
-      <div className='gel-layout__item'>
-        <span className='message gel-double-pica'>Attempting to get get data, if a problem arrises please check the following:</span>
-        <ul>
-          <li>Check the console for any errors</li>
-          <li>URL contains <b>base-url=</b><a href='#'>{'http://locaton:port'}</a> where your data is being served, it defaults to <a href='http://ipstudio-discovery.rd.bbc.co.uk:8870'>{'http://ipstudio-discovery.rd.bbc.co.uk:8870'}</a></li>
-          <li>Check your servers are working</li>
-        </ul>
-      </div>
-    </div>
-    <div className='gel-layout'>
-      {view.errored.map((errored, index) => {
-        return <Button
-          key={`errored-${index}`}
-          name='errored'
-          className='status'
-          icon={<No />}
-          label={errored} />
-      })}
+let Loading = ({view}) => {
+  return <Layout className={view.status} layouts='auto'>
+    <Layout className={view.status} layouts='center' gels='1/1'>
+      <LayoutItem className='header'>
+        <Trafalgar className='title title-loading'><Spinner /> Loading</Trafalgar>
+        <Trafalgar className='title title-error'><No /> Error: Exceeded retry limit</Trafalgar>
+      </LayoutItem>
+    </Layout>
+    <LayoutItem>
+      <GreatPrimer>Attempting to get get data, if a problem arrises please check the following:</GreatPrimer>
+      <ul>
+        <li><Pica>Check the console for any errors</Pica></li>
+        <li><Pica>URL contains <b>base-url=</b><a href='#'>{'http://locaton:port'}</a> where your data is being served, it defaults to <a href='http://ipstudio-discovery.rd.bbc.co.uk:8870'>{'http://ipstudio-discovery.rd.bbc.co.uk:8870'}</a></Pica></li>
+        <li><Pica>Check your servers are working</Pica></li>
+      </ul>
+    </LayoutItem>
+    <Layout layouts='center' gels='1/1'>
       {view.notLoaded.map((notLoaded, index) => {
-        return <Button
-          key={`not-loaded-${index}`}
-          name='not-loaded'
-          className='status'
-          icon={<Loading />}
-          label={notLoaded} />
+        return <Status key={`not-loaded-${index}`} label={notLoaded} icon={<Spinner />} />
+      })}
+      {view.errored.map((errored, index) => {
+        return <Status key={`not-errored-${index}`} label={errored} icon={<No />} />
       })}
       {view.loaded.map((loaded, index) => {
-        return <Button
-          key={`loaded-${index}`}
-          name='loaded'
-          className='status'
-          icon={<Yes />}
-          label={loaded} />
+        return <Status key={`loaded-${index}`} label={loaded} icon={<Yes />} />
       })}
-    </div>
-  </div>
+    </Layout>
+  </Layout>
 }
 
-LoadingComponent.propTypes = {
+Loading.propTypes = {
   view: PropTypes.object.isRequired
 }
 
-export default LoadingComponent
+export default Loading
