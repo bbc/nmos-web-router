a1f6aa0c4340eaa83c771608506b84a0fe740f38
Luke Preston <luke.preston@bbc.co.uk>
2016-10-04 15:54:33 +0100

    renaming to logo and changing to 555 black

--- src/main/components/logo.js
+++ src/main/components/logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => {
   return <svg
-    className='icon'
+    className='logo'
     version='1.1'
     xmlns='http://www.w3.org/2000/svg'
     viewBox='0 0 283.46 283.46'>
