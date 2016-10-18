43dbe4ec5ecdba28957ba3cbe208dbadfdae1d92
Luke Preston <luke.preston@bbc.co.uk>
2016-10-18 08:22:55 +0100

    extracting collection

--- src/ips-api/stub/collections.js
+++ src/ips-api/stub/collections.js
@@ -1,5 +1,6 @@
 import Loki from 'lokijs'
-import stripLoki from './strip-loki'
+
+import Collection from './collection'
 
 let types = [
   'subscriptions',
@@ -10,37 +11,10 @@ let types = [
   'receivers'
 ]
 
-function createCollection (db, type) {
-  let collection = db.addCollection(type)
-  let data = require(`./data/${type}.json`)
-  collection.insert(data)
-
-  collection.find = (query) => {
-    let data = collection.findOne(query)
-    if (data !== null) return stripLoki(data)
-    return data
-  }
-
-  collection.all = () => {
-    return collection.data.map(stripLoki)
-  }
-
-  collection.modify = (item, changes) => {
-    collection.emit('pre', item)
-    item = Object.assign({}, item, changes)
-    collection.update(item)
-  }
-
-  return collection
-}
-
 function createCollections () {
   let db = new Loki('ips.json')
   let collections = {}
-  types.forEach(type => {
-    collections[type] = createCollection(db, type)
-  })
-  collections.db = db
+  types.forEach(type => { collections[type] = Collection(db, type) })
   return collections
 }
 
