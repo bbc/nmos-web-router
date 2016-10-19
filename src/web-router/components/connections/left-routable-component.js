8eeec1cf2219e7a7761a0d696c16eec0b6557eb3
Luke Preston <luke.preston@bbc.co.uk>
2016-10-19 13:40:06 +0100

    full things are looking OK

--- src/web-router/components/connections/left-routable-component.js
+++ src/web-router/components/connections/left-routable-component.js
@@ -1,19 +1,38 @@
 import React, { PropTypes } from 'react'
 import Icon from './icon-component'
 import Button from '../../../components/button-component'
-import { LayoutItem } from '../../../gel-react/grid'
+import { Layout, LayoutItem } from '../../../gel-react/grid'
 
 let LeftRoutable = ({routable, actions}) => {
   return <LayoutItem gels='1/1' className={`routable ${routable.state}`}>
-    <Button
-      onClick={function () {
-        actions.toggleLeft(routable)
-      }}
-      fill
-      icon={<Icon format={routable.format} />}
-      label={routable.label}
-      />
-    <div className={`node ${routable.node.state}`} />
+    <div className='short'>
+      <Button
+        onClick={function () {
+          actions.toggleLeft(routable)
+        }}
+        fill
+        icon={<Icon format={routable.format} />}
+        label={routable.label}
+        />
+      <div className={`node ${routable.node.state}`} />
+    </div>
+    <div className='full'>
+      <div className='button-container'>
+        <Layout
+          className='button'
+          onClick={function () {
+            actions.toggleLeft(routable)
+          }}>
+          <LayoutItem gels='1/1'>
+            <Icon format={routable.format} />
+          </LayoutItem>
+          <LayoutItem gels='1/1'>
+            <span className='label'>word word word word word word word word word word word word word word word word word </span>
+          </LayoutItem>
+        </Layout>
+      </div>
+      <div className={`node ${routable.node.state}`} />
+    </div>
   </LayoutItem>
 }
 
