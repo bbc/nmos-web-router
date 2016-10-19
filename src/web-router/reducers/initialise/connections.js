88573e5401ed34e98604833afb08c813cad2bf9e
Luke Preston <luke.preston@bbc.co.uk>
2016-10-19 09:28:19 +0100

    routes are applied

--- src/web-router/reducers/initialise/connections.js
+++ src/web-router/reducers/initialise/connections.js
@@ -1,19 +1,27 @@
-function defaultRoutable (routable) {
+function isRouted (side, routes, routable) {
+  return routes.filter(route => {
+    return route[side].id === routable.id
+  }).length > 0
+}
+
+function defaultRoutable (side, routes, routable) {
   routable.state = 'contracted'
   routable.node = {
-    status: 'unrouted',
-    deletable: false,
-    visible: true
+    status: isRouted(side, routes, routable) ? 'routed' : 'unrouted'
   }
   return routable
+}
+
+function mapRoutables (data, routes, sides, side) {
+  return data[sides[side].plural].map(routable => {
+    return defaultRoutable(sides[side].singular, routes, routable)
+  })
 }
 
 export default (data, view, sides) => {
   let connections = Object.assign({}, view.connections)
   connections.routes = Object.assign([], data.routes)
-
-  connections.routables.left = data[sides.left.plural].map(defaultRoutable)
-  connections.routables.right = data[sides.right.plural].map(defaultRoutable)
-
+  connections.routables.left = mapRoutables(data, connections.routes, sides, 'left')
+  connections.routables.right = mapRoutables(data, connections.routes, sides, 'right')
   return connections
 }
