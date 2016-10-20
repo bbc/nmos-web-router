f2649bcd87394154bbc8e9952266d039fb0e16c9
Luke Preston <luke.preston@bbc.co.uk>
2016-10-20 10:51:29 +0100

    renaming some left stuff to sender

--- src/web-router/dispatchers/index.js
+++ src/web-router/dispatchers/index.js
@@ -1,7 +1,7 @@
 import initialise from './initialise-dispatcher'
 import update from './update-dispatcher'
 import initialiseError from './initialise-error-dispatcher'
-import toggleSender from './connections/toggle-left-dispatcher'
+import toggleSender from './connections/toggle-sender-dispatcher'
 import unroute from './connections/unroute-dispatcher'
 import route from './connections/route-dispatcher'
 
--- src/web-router/reducers/index.js
+++ src/web-router/reducers/index.js
@@ -1,6 +1,6 @@
 import initialise from './initialise'
 import initialiseError from './initialise-error-reducer'
-import toggleSender from './connections/toggle-left-reducer'
+import toggleSender from './connections/toggle-sender-reducer'
 import unroute from './connections/unroute-reducer'
 import route from './connections/route-reducer'
 import update from './update'
