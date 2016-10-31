fe9da97c5df88a3801624aaacad90008c0b7cb73
Luke Preston <luke.preston@bbc.co.uk>
2016-10-31 15:19:28 +0000

    have route view in place

--- src/web-router/components/index.js
+++ src/web-router/components/index.js
@@ -1,23 +1,25 @@
 import './web-router.css'
 
 import React, { PropTypes } from 'react'
-import { Wrap, Layout } from '../../gel-react/grid'
 import Connections from './connections'
+import { DoublePica } from '../../gel-react/typography'
+import { LayoutItem } from '../../gel-react/grid'
 
 let WebRouter = ({data, view, actions}) => {
   let webRouterContainer = document.querySelector('.web-router-container')
   if (webRouterContainer !== null && view.scroll) webRouterContainer.scrollTop = 0
 
-  return <Layout className={'web-router web-router-connectons'}>
-    <Layout gels='1/1'>
-      <Wrap className='web-router-container'>
-        <Connections
-          view={view.connections}
-          actions={actions}
-          />
-      </Wrap>
-    </Layout>
-  </Layout>
+  return <div className='web-router web-router/choose/route/confirm'>
+    <div className='container choose left'></div>
+    <div className='container route center'>
+      <LayoutItem><DoublePica>Route</DoublePica></LayoutItem>
+      <Connections
+        view={view.connections}
+        actions={actions}
+        />
+    </div>
+    <div className='container confirm right'>CONFIRM</div>
+  </div>
 }
 
 WebRouter.propTypes = {
