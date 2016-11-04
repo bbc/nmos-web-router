e402166e2097331b18982f5542b7dbc37bb1bd71
Luke Preston <luke.preston@bbc.co.uk>
2016-11-04 15:55:54 +0000

    checking updates all visible

--- src/web-router/reducers/choose/check-reducer.js
+++ src/web-router/reducers/choose/check-reducer.js
@@ -1,3 +1,16 @@
+function allVisible (routables) {
+  let uncheckedSenders = 0
+  let filteredSenders = 0
+  routables.forEach(routable => {
+    if (routable.state.includes('uncheck')) uncheckedSenders += 1
+    if (routable.state.includes('fuzzymatch')) filteredSenders += 1
+  })
+
+  if (uncheckedSenders === 0) return 'all'
+  else if (uncheckedSenders !== filteredSenders) return 'some'
+  return 'none'
+}
+
 export default (state, action, merge) => {
   let type = action.routable.type
   let view = Object.assign({}, state.view)
@@ -7,5 +20,7 @@ export default (state, action, merge) => {
     })[0]
   if (routable.state.includes('unchecked')) routable.state = routable.state.replace('unchecked', 'checked')
   else routable.state = routable.state.replace('checked', 'unchecked')
+  view.choose.allVisibleState.senders = allVisible(view.senders)
+  view.choose.allVisibleState.receivers = allVisible(view.receivers)
   return merge({ view })
 }
