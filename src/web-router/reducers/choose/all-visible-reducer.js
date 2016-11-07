08af3b3a2e86857f306578ad44a72b82fc7bdf5c
Luke Preston <luke.preston@bbc.co.uk>
2016-11-07 13:52:06 +0000

    all checked correctly this time

--- src/web-router/reducers/choose/all-visible-reducer.js
+++ src/web-router/reducers/choose/all-visible-reducer.js
@@ -4,28 +4,14 @@ export default (state, action, merge) => {
   let type = action.routableType
   let view = Object.assign({}, state.view)
   let choose = view.choose
-
-  let checked = 0
-  let unchecked = 0
-  let matched = 0
-  view[type].forEach(routable => {
-    if (routable.state.includes('fuzzymatch')) {
-      matched += 1
-      if (routable.state.includes('unchecked')) unchecked += 1
-      else checked += 1
-    }
-  })
-
-  if (checked === matched) choose.allVisibleState[type] = 'none'
-  else if (unchecked === matched) choose.allVisibleState[type] = 'all'
-  else if (checked > unchecked) choose.allVisibleState[type] = 'all'
-  else choose.allVisibleState[type] = 'none'
+  if (choose.allVisibleState[type] === 'all') choose.allVisibleState[type] = 'none'
+  else if (choose.allVisibleState[type] === 'some') choose.allVisibleState[type] = 'all'
+  else if (choose.allVisibleState[type] === 'none') choose.allVisibleState[type] = 'all'
 
   view[type].forEach(routable => {
     let changeState = ChangeState(routable)
-    if (routable.state.includes('fuzzymatch'))
-      if (choose.allVisibleState[type] === 'all') changeState.check()
-      else if (choose.allVisibleState[type] === 'none' && !routable.state.includes('unchecked')) changeState.uncheck()
+    if (choose.allVisibleState[type] === 'all') changeState.check()
+    else if (choose.allVisibleState[type] === 'none' && !routable.state.includes('unchecked')) changeState.uncheck()
   })
 
   return merge ({ view })
