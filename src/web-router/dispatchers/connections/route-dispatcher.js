1ecc7c3556b6e8199c74e921737febd7b22fe16f
Luke Preston <luke.preston@bbc.co.uk>
2016-11-17 13:19:29 +0000

    removing things which I added to do the routeing

--- src/web-router/dispatchers/connections/route-dispatcher.js
+++ src/web-router/dispatchers/connections/route-dispatcher.js
@@ -3,6 +3,11 @@ export default (actions) => {
     let sender = senders.filter(receiver => {
       return receiver.state.includes('expanded')
     })[0]
+    sender = Object.assign({}, sender)
+    delete sender.node
+    delete sender.nodeEl
+    delete sender.format
+    delete sender.state
     actions.route({ receiver, sender })
     window.nmos.route(receiver.id, sender)
   }
