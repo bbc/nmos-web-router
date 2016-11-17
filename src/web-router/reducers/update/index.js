69998f14c1ad2ec003d7c56855d8cccd2526878e
Luke Preston <luke.preston@bbc.co.uk>
2016-11-17 13:50:35 +0000

    removed the logging

--- src/web-router/reducers/update/index.js
+++ src/web-router/reducers/update/index.js
@@ -1,18 +1,8 @@
 import updateData from './data'
 import updateView from './view'
-import onGrain from './on-grain'
 
 export default (state, action, merge) => {
-  if (action.name === 'view') {
-    return merge({ view: Object.assign({}, state.view) })
-  }
-  let stateData = Object.assign({}, state.data)
-  onGrain(stateData[action.name], action.update[action.name], {
-    update () { console.log('update:', action.name) },
-    add () { console.log('add:', action.name) },
-    remove () { console.log('remove:', action.name) },
-    unknown () { console.log('unknown:', action.name) }
-  })
+  if (action.name === 'view') return merge({ view: Object.assign({}, state.view) })
   let data = updateData(state, action)
   let view = Object.assign({}, state.view)
   view = updateView(data, view, action)
