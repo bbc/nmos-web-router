ae33e0b48e5ab5bdbb2b5c79abbf7da349e8823c
Luke Preston <luke.preston@bbc.co.uk>
2016-11-18 12:32:12 +0000

    can add to view

--- src/web-router/reducers/update/view.js
+++ src/web-router/reducers/update/view.js
@@ -1,4 +1,43 @@
+import fuzzysearch from 'fuzzysearch'
 import ChangeState from '../change-state'
+
+function isSenderRouted (sender, receivers) {
+  return receivers.filter(receiver => {
+    return receiver.subscription.sender_id === sender.id
+  }).length > 0
+}
+
+function fuzzymatch (view, routable, changeState) {
+  let fuzzymatch = fuzzysearch(view.choose.term.toLowerCase(), routable.label.toLowerCase()) || fuzzysearch(view.choose.term.toLowerCase(), routable.id.toLowerCase())
+  if (fuzzymatch) changeState.fuzzymatch()
+  else changeState.fuzzymissmatch()
+  return routable
+}
+
+function mapSender (sender, data, view) {
+  sender = Object.assign({}, sender)
+  let changeState = ChangeState(sender)
+  changeState.check().contract().selectable()
+  if (isSenderRouted(sender, data.receivers)) changeState.route()
+  else changeState.unroute()
+  sender = fuzzymatch(view, sender, changeState)
+  return sender
+}
+
+function mapReceiver (receiver, view, senders) {
+  receiver = Object.assign({}, receiver)
+  let changeState = ChangeState(receiver)
+  changeState.check().contract().notSelectable()
+  if (receiver.subscription.sender_id !== null) {
+    let sender = senders.filter(sender => {
+      return sender.id === receiver.subscription.sender_id
+    })[0]
+    receiver.subscription.sender = sender
+    changeState.route()
+  } else changeState.unroute()
+  receiver = fuzzymatch(view, receiver, changeState)
+  return receiver
+}
 
 export default (data, view, action) => {
   if (action.name === 'flows') return view
@@ -22,8 +61,14 @@ export default (data, view, action) => {
       return r.id === routable.id
     })[0]
 
-    if (matched === undefined) console.log('add new thing')
+    if (matched === undefined && action.name === 'receivers') {
+      view[action.name].push(mapReceiver(routable, view, view.senders))
+    } else if (matched === undefined && action.name === 'senders') {
+      view[action.name].push(mapSender(routable, data, view))
+    }
   })
+
+  view[action.name].sort(window.nmos.defaultSort)
 
   return view
 }
