2175d45aff2fed6fcc85254d0fd5e8010fbde98f
Luke Preston <luke.preston@bbc.co.uk>
2016-11-18 09:50:06 +0000

    unroute was calling remove

--- src/web-router/reducers/connections/unroute-reducer.js
+++ src/web-router/reducers/connections/unroute-reducer.js
@@ -4,7 +4,7 @@ export default (state, action, merge) => {
   let view = Object.assign({}, state.view)
   view.receivers = view.receivers.map(routable => {
     let changeState = ChangeState(routable)
-    if (routable.id === action.receiver.id) changeState.remove()
+    if (routable.id === action.receiver.id) changeState.unroute()
     return routable
   })
   return merge({ view })
