7d2d338d9e85176864f439ffe080e22b812dedac
Luke Preston <luke.preston@bbc.co.uk>
2016-11-21 12:40:12 +0000

    expanded route updates it routed state

--- src/web-router/reducers/update/view.js
+++ src/web-router/reducers/update/view.js
@@ -146,6 +146,23 @@ export default (data, view, action) => {
     return updateSenderRoutes(sender, data)
   })
 
+  let expandedId = view.expandedSender.id
+  let matchSender = view.senders.filter(sender => {
+    return sender.id === expandedId
+  })[0]
+  if (matchSender) {
+    view.expandedSender.id = matchSender.id
+    view.expandedSender.label = matchSender.label
+    view.expandedSender.description = matchSender.description
+    view.expandedSender.format = matchSender.format
+
+    let changeState = ChangeState(view.expandedSender)
+    if (matchSender.node.state.includes('routed')) changeState.route()
+    else changeState.unroute()
+  } else {
+    view.expandedSender.contract()
+  }
+
   view[action.name].sort(window.nmos.defaultSort)
 
   return view
