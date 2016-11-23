45d7fa6f5df3cd4fd8082407f695d0b233902c7d
Luke Preston <luke.preston@bbc.co.uk>
2016-11-23 12:15:18 +0000

    initial locaiton change test

---
+++ src/web-router/reducers/locaton-change-reducer.spec.js
@@ -0,0 +1,68 @@
+import merge from '../../mock-merge'
+import locationChangeReducer from './location-change-reducer'
+
+const initialState = {
+  view: {
+    choose: {
+      allVisibleState: {}
+    },
+    location: '',
+    senders: [{
+      state: ['', '', 'uncheck'],
+      id: 'xxxx-xxx-xxx-xxxx',
+      label: 'label-a'
+    }, {
+      state: ['', '', 'uncheck'],
+      id: 'xxxx-xxx-xxx-xxxx',
+      label: 'label-b'
+    }],
+    receivers: [{
+      state: ['', '', 'uncheck'],
+      id: 'xxxx-xxx-xxx-xxxx',
+      label: 'label-a'
+    }, {
+      state: ['', '', 'uncheck'],
+      id: 'xxxx-xxx-xxx-xxxx',
+      label: 'label-b'
+    }]
+  }
+}
+
+describe('Location Change Reducer', () => {
+  it('Does nothing if the pathname does not include web-router', () => {
+    let newState = locationChangeReducer(initialState, {
+      payload: {
+        pathname: '',
+        query: {}
+      }
+    }, merge)
+
+    expect(newState.view.senders).toEqual(initialState.view.senders)
+    expect(newState.view.receivers).toEqual(initialState.view.receivers)
+  })
+
+  it('Updates the location when pathname includes /web-router', () => {
+    let newState = locationChangeReducer(initialState, {
+      payload: {
+        pathname: '/web-router/location',
+        query: {}
+      }
+    }, merge)
+
+    expect(newState.view.location).toBe('/location')
+  })
+
+  // it('Filters based on search parameter', () => {
+  //   let newState = locationChangeReducer(initialState, {
+  //     payload: {
+  //       pathname: '/web-router/',
+  //       query: {
+  //         search: 'label-a'
+  //       }
+  //     }
+  //   }, merge)
+  //
+  //   expect(newState.view.choose.allVisibleState.senders).toBe('some')
+  //   expect(newState.view.choose.allVisibleState.receivers).toBe('some')
+  // })
+})
