8b43675d6bea56b469f58a40c8fc3d92115b8bab
Luke Preston <luke.preston@bbc.co.uk>
2016-11-24 13:43:45 +0000

    removing button from navigation

--- src/main/components/welcome-compoment.jsx
+++ src/main/components/welcome-compoment.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
-
-import Button from '../../components/button-component'
+import {Link} from 'react-router'
 import { Layout, LayoutItem } from '../../gel-react/grid'
 import { Trafalgar, GreatPrimer } from '../../gel-react/typography'
 
 let Welcome = () => {
   return <Layout layouts='center' className='welcome'>
-    <LayoutItem>
+    <LayoutItem gels='9/10'>
       <Trafalgar>Welcome to the IP Studio site</Trafalgar>
     </LayoutItem>
-    <LayoutItem>
+    <LayoutItem gels='9/10'>
       <GreatPrimer>You will find all the utilities to interact and observe IP Studio</GreatPrimer>
     </LayoutItem>
-    <LayoutItem>
-      <Button fill to='web-router' name='contents' label='web router' />
+    <LayoutItem gels='9/10'>
+      <Link
+        style={{
+          width: '100%',
+          textAlign: 'center'
+        }}
+        to='/web-router'
+        className='button contents'>
+        WEB ROUTER
+      </Link>
     </LayoutItem>
   </Layout>
 }
