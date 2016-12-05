a0f3423b5ff8136f16b4e2e39e8020f8c41f5a39
Luke Preston <luke.preston@bbc.co.uk>
2016-12-05 11:10:06 +0000

    changing location will take you to correct location

--- src/web-router/reducers/location-change-reducer.js
+++ src/web-router/reducers/location-change-reducer.js
@@ -1,32 +1,12 @@
-// import filter from './filter'
-//
-// function allVisible (routables) {
-//   let unchecked = 0
-//   let filtered = 0
-//   routables.forEach(routable => {
-//     if (routable.state.includes('fuzzymatch')) {
-//       filtered += 1
-//       if (routable.state.includes('uncheck')) unchecked += 1
-//     }
-//   })
-//   if (filtered === 0) return 'none'
-//   else if (unchecked === 0) return 'all'
-//   else if (unchecked !== filtered) return 'some'
-//   return 'none'
-// }
-
 export default (state, action, merge) => {
-  // let pathname = action.payload.pathname
-  // let query = action.payload.query
-  // let view = Object.assign({}, state.view)
-  // if (pathname.includes('/web-router/')) {
-  //   view.location = pathname.replace('/web-router', '')
-  //   if (query.hasOwnProperty('search')) {
-  //     view.choose.term = query.search
-  //     view = filter(view)
-  //   }
-  // }
-  // if (view.senders.length !== 0) view.choose.allVisibleState.senders = allVisible(view.senders)
-  // if (view.receivers.length !== 0) view.choose.allVisibleState.receivers = allVisible(view.receivers)
-  // return merge({ view })
+  let pathname = action.payload.pathname
+  let query = action.payload.query
+  let view = Object.assign({}, state.view)
+  if (pathname.includes('/web-router/')) {
+    view.location = pathname.replace('/web-router', '')
+    if (query.hasOwnProperty('search')) {
+      view.choose.term = query.search
+    }
+  }
+  return merge({ view })
 }
