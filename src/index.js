2ae96a406aed36924f85c86bfc145525464ec350
Luke Preston <luke.preston@bbc.co.uk>
2016-12-12 13:36:14 +0000

    using a parsed url func to make getting query params easier

--- src/index.js
+++ src/index.js
@@ -6,24 +6,64 @@ import NMOS from './ips-nmos-api/src'
 import main from './main'
 import webRouter from './web-router'
 
-const usestub = window.location.search.includes('stub')
-const priority = window.location.search.includes('priority')
+function parseURL (url) {
+  let parser = document.createElement('a')
+  parser.href = url
 
-function queryUrl (defaultUrl) {
-  if (window.location.search.includes(`url=`)) {
-    return window.location.search
-    .replace('?', '')
-    .split('&')
-    .filter(query => {
-      return query.includes(`url=`)
-    })
-    .map(query => {
-      return query.replace(`url=`, '')
-    })[0]
-  } else return defaultUrl
+  let query = {}
+  let queries = parser.search.replace(/^\?/, '').split('&')
+  for (let i = 0; i < queries.length; i++) {
+    let split = queries[i].split('=')
+    let value = split[1] || ''
+    query[split[0]] = {}
+    query[split[0]].string = value
+    query[split[0]].decode = decodeURIComponent(value)
+    query[split[0]].number = parseInt(value)
+    query[split[0]].boolean = value === 'true' || value === ''
+  }
+  return {
+    protocol: parser.protocol,
+    host: parser.host,
+    hostname: parser.hostname,
+    port: parser.port,
+    pathname: parser.pathname,
+    search: parser.search,
+    query: function (name) {
+      let defaultQuery = {
+        boolean: false,
+        number: NaN,
+        string: '',
+        decode: ''
+      }
+      if (!query.hasOwnProperty(name)) return defaultQuery
+      return query[name]
+    },
+    hash: parser.hash
+  }
 }
 
-function defaultUrl (representations) {
+const parsedUrl = parseURL(window.location)
+const queryUrl = parsedUrl.query('url').string
+const queryStub = parsedUrl.query('stub').boolean
+const queryPriority = parsedUrl.query('priority').number
+
+function start (stub, url) {
+  window.nmos = NMOS({
+    stub,
+    get: url
+  })
+
+  drc({
+    '/': {
+      component: main,
+      'web-router(/**)': {
+        component: webRouter
+      }
+    }
+  }, true)
+}
+
+function topPriority (representations) {
   let lessThanOneHundred = representations
     .filter(representation => {
       return representation.priority <= 100
@@ -37,43 +77,29 @@ function defaultUrl (representations) {
   return `http://${representation.address}:${representation.port}`
 }
 
-let host = window.location.host
-host = host.split(':')[0]
-
-axios
-  .get(`http://${host}:12345/x-nmos/node/v1.0/self/`)
-  .then(result => {
-    let service = result.data.services.filter(service => {
-      return service.type === 'urn:x-ipstudio:service:mdnsbridge'
-    })[0]
-    let url = service.href + 'nmos-query/'
-    return axios.get(url)
-  })
-  .then(result => {
-    let representations = result.data.representation
-    let url = defaultUrl(representations)
-    if (priority) {
-      let match = window.location.search.match(/\w*priority=(\d*)/g)[0]
-      let priority = parseInt(match.split('=')[1])
-      let representation = representations
-        .filter(representation => {
-          return representation.priority === priority
-        })[0]
-      if (representation) {
-        url = `http://${representation.address}:${representation.port}`
-      }
-    }
-
-    window.nmos = NMOS({
-      stub: usestub,
-      get: queryUrl(url)
+if (queryStub || queryUrl) start(queryStub, queryUrl)
+else {
+  axios
+    .get(`http://${parsedUrl.hostname}:12345/x-nmos/node/v1.0/self/`)
+    .then(result => {
+      let service = result.data.services.filter(service => {
+        return service.type === 'urn:x-ipstudio:service:mdnsbridge'
+      })[0]
+      let url = service.href + 'nmos-query/'
+      return axios.get(url)
     })
-    drc({
-      '/': {
-        component: main,
-        'web-router(/**)': {
-          component: webRouter
+    .then(result => {
+      let representations = result.data.representation
+      let url = topPriority(representations)
+      if (queryPriority) {
+        let representation = representations
+        .filter(representation => {
+          return representation.priority === queryPriority
+        })[0]
+        if (representation) {
+          url = `http://${representation.address}:${representation.port}`
         }
       }
-    }, true)
-  })
+      start(queryStub, url)
+    })
+}
