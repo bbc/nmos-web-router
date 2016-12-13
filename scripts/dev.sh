97051857ace3c084e450975c18075693dca15dff
Luke Preston <luke.preston@bbc.co.uk>
2016-12-13 14:55:56 +0000

    dev will start mdnsbridge and nmos

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,2 +1,4 @@
-[ -z `docker ps -aqf 'name=ips-web-dev-start'` ] || docker rm -f `docker ps -aqf 'name=ips-web-dev-start'`
-docker run -d -p 3000:3000 --name ips-web-dev-start -v `pwd`/src:/usr/src/app/src ips-web npm start
+[ -z `docker ps -aqf 'name=ips-web-dev'` ] || docker rm -f `docker ps -aqf 'name=ips-web-dev'`
+docker run -d -p 3000:3000 -p 12345:12345 -p 6589:6589 -p 6590:6590 -p 6591:6591 --name ips-web-dev -v `pwd`/src:/usr/src/app/src ips-web npm start
+docker exec -dit `docker ps -aqf 'name=ips-web-dev'` npm run mdnsbridge
+docker exec -dit `docker ps -aqf 'name=ips-web-dev'` npm run nmos
