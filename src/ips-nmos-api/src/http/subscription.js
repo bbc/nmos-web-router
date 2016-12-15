54b90ed0c5f68035a90b665491499ab4a576fb8a
Luke Preston <luke.preston@bbc.co.uk>
2016-12-15 15:30:03 +0000

    extracting poll and sockets to functions

--- src/ips-nmos-api/src/http/subscription.js
+++ src/ips-nmos-api/src/http/subscription.js
@@ -42,6 +42,85 @@ module.exports = (getters, WebSocket, subscriptions, type) => {
       })
   }
 
+  let poll = () => {
+    clearInterval(polling.intervalToken)
+    clearTimeout(polling.subscriptionToken)
+    status = 'polling'
+    pollings.forEach(callback => {
+      callback()
+    })
+    polling.intervalToken = setInterval(() => {
+      get()
+    }, polling.interval)
+    polling.subscriptionToken = setTimeout(connect, polling.subscriptionsInterval)
+  }
+
+  let sockets = (subscription) => {
+    ws = new WebSocket(subscription.ws_href)
+    if (typeof ws.onopen !== undefined) {
+      ws.onopen = () => {
+        status = 'opened'
+        opens.forEach(callback => {
+          callback()
+        })
+      }
+    } else {
+      ws.on('open', () => {
+        status = 'opened'
+        opens.forEach(callback => {
+          callback()
+        })
+      })
+    }
+
+    if (typeof ws.onmessage !== undefined) {
+      ws.onmessage = (evt) => {
+        let data = JSON.parse(evt.data)
+        updates.forEach(callback => {
+          callback(data)
+        })
+      }
+    } else {
+      ws.on('message', (data) => {
+        updates.forEach(callback => {
+          callback(data)
+        })
+      })
+    }
+
+    if (typeof ws.onclose !== undefined) {
+      ws.onclose = () => {
+        status = 'closed'
+        closes.forEach(callback => {
+          callback()
+        })
+      }
+    } else {
+      ws.on('close', () => {
+        status = 'closed'
+        closes.forEach(callback => {
+          callback()
+        })
+      })
+    }
+
+    if (typeof ws.onerror !== undefined) {
+      ws.onerror = (error) => {
+        status = 'errored'
+        errors.forEach(callback => {
+          callback(error)
+        })
+      }
+    } else {
+      ws.on('error', (error) => {
+        status = 'errored'
+        errors.forEach(callback => {
+          callback(error)
+        })
+      })
+    }
+  }
+
   let connect = () => {
     clearInterval(polling.intervalToken)
     clearTimeout(polling.subscriptionToken)
@@ -50,93 +129,12 @@ module.exports = (getters, WebSocket, subscriptions, type) => {
         let subscription = subscriptions.filter(subscription => {
           return subscription.resource_path === `/${type}`
         })[0]
-        if (subscription === undefined) {
-          status = 'polling'
-          pollings.forEach(callback => {
-            callback()
-          })
-          polling.intervalToken = setInterval(() => {
-            get()
-          }, polling.interval)
-          polling.subscriptionToken = setTimeout(connect, polling.subscriptionsInterval)
-        } else {
-          ws = new WebSocket(subscription.ws_href)
-          if (typeof ws.onopen !== undefined) {
-            ws.onopen = () => {
-              status = 'opened'
-              opens.forEach(callback => {
-                callback()
-              })
-            }
-          } else {
-            ws.on('open', () => {
-              status = 'opened'
-              opens.forEach(callback => {
-                callback()
-              })
-            })
-          }
-
-          if (typeof ws.onmessage !== undefined) {
-            ws.onmessage = (evt) => {
-              let data = JSON.parse(evt.data)
-              updates.forEach(callback => {
-                callback(data)
-              })
-            }
-          } else {
-            ws.on('message', (data) => {
-              updates.forEach(callback => {
-                callback(data)
-              })
-            })
-          }
-
-          if (typeof ws.onclose !== undefined) {
-            ws.onclose = () => {
-              status = 'closed'
-              closes.forEach(callback => {
-                callback()
-              })
-            }
-          } else {
-            ws.on('close', () => {
-              status = 'closed'
-              closes.forEach(callback => {
-                callback()
-              })
-            })
-          }
-
-          if (typeof ws.onerror !== undefined) {
-            ws.onerror = (error) => {
-              status = 'errored'
-              errors.forEach(callback => {
-                callback(error)
-              })
-            }
-          } else {
-            ws.on('error', (error) => {
-              status = 'errored'
-              errors.forEach(callback => {
-                callback(error)
-              })
-            })
-          }
-        }
+        if (subscription === undefined) poll()
+        else sockets(subscription)
       })
       .catch((error) => {
         console.error(error)
-        clearInterval(polling.intervalToken)
-        clearTimeout(polling.subscriptionToken)
-        status = 'polling'
-        pollings.forEach(callback => {
-          callback()
-        })
-        polling.intervalToken = setInterval(() => {
-          get()
-        }, polling.interval)
-        polling.subscriptionToken = setTimeout(connect, polling.subscriptionsInterval)
+        poll()
       })
   }
 
