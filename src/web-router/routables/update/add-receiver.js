e71628abd9273f5c6e0eef6619b975f437e23e8e
Luke Preston <luke.preston@bbc.co.uk>
2017-01-17 13:38:41 +0000

    adds receivers on update

--- src/web-router/routables/update/add-receiver.js
+++ src/web-router/routables/update/add-receiver.js
@@ -1,2 +1,7 @@
+import insertReceivers from '../insert/insert-receivers'
+import insertRoutes from '../insert/insert-routes'
+
 export default ({data, grain}) => {
+  insertReceivers(data, [grain.post])
+  insertRoutes(data)
 }
