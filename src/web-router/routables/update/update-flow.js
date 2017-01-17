204c4e51057851debf6e3d5e4c6ad91ecfa4f59b
Luke Preston <luke.preston@bbc.co.uk>
2017-01-17 09:44:08 +0000

    adding update flow test

--- src/web-router/routables/update/update-flow.js
+++ src/web-router/routables/update/update-flow.js
@@ -1,2 +1,9 @@
+import mapSenderFormats from '../common/map-sender-formats'
+
 export default ({data, grain}) => {
+  data.flows.forEach((flow, index) => {
+    if (flow.id === grain.pre.id) data.flows[index] = grain.post
+  })
+
+  mapSenderFormats(data)
 }
