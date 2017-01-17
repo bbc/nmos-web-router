9834779fc661ce439194db73f0917775dbc69b84
Luke Preston <luke.preston@bbc.co.uk>
2017-01-17 13:28:19 +0000

    making routes consistent in insert

--- src/web-router/routables/insert/index.js
+++ src/web-router/routables/insert/index.js
@@ -1,6 +1,6 @@
 import cloneRoutables from '../common/clone-routables'
 import Routables from '..'
-import mapInitialRouted from './map-initial-routed'
+import insertRoutes from './insert-routes'
 import insertSenders from './insert-senders'
 import insertReceivers from './insert-receivers'
 import insertFlows from './insert-flows'
@@ -11,12 +11,12 @@ export default (data) => {
   return {
     senders (senders) {
       insertSenders(data, senders)
-      data.routes = mapInitialRouted(data)
+      insertRoutes(data)
       return Routables(data)
     },
     receivers (receivers) {
       insertReceivers(data, receivers)
-      data.routes = mapInitialRouted(data)
+      insertRoutes(data)
       return Routables(data)
     },
     flows (flows) {
--- src/web-router/routables/insert/insert-routes.js
+++ src/web-router/routables/insert/insert-routes.js
@@ -1,23 +1,23 @@
 import clone from 'clone'
 import sortRoutes from '../common/sort-routes'
 
-export default ({senders, receivers}) => {
+export default (data) => {
   function get (routables, id) {
     return routables.filter(routable => {
       return routable.id === id
     })[0]
   }
 
-  let routes = receivers
+  data.routes = data.receivers
     .filter(receiver => {
       return receiver.subscription.sender_id !== null
     })
     .filter(receiver => {
       // this is not a good thing... but needed for sorting
-      return get(senders, receiver.subscription.sender_id) !== undefined
+      return get(data.senders, receiver.subscription.sender_id) !== undefined
     })
     .map(receiver => {
-      let sender = get(senders, receiver.subscription.sender_id)
+      let sender = get(data.senders, receiver.subscription.sender_id)
       return {
         state: 'routed',
         receiver: clone(receiver),
@@ -25,6 +25,5 @@ export default ({senders, receivers}) => {
       }
     })
 
-  routes.sort(sortRoutes)
-  return routes
+  data.routes.sort(sortRoutes)
 }
