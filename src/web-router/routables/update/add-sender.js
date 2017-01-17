82a3e2915d54c76b8b7536005cfe5f5338a5939e
Luke Preston <luke.preston@bbc.co.uk>
2017-01-17 13:49:54 +0000

    can add senders on update

--- src/web-router/routables/update/add-sender.js
+++ src/web-router/routables/update/add-sender.js
@@ -1,2 +1,7 @@
+import insertSenders from '../insert/insert-senders'
+import insertRoutes from '../insert/insert-routes'
+
 export default ({data, grain}) => {
+  insertSenders(data, [grain.post])
+  insertRoutes(data)
 }
