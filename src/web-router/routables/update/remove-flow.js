ea19b6742359cfd414aa3121760fda29d5f22b77
Luke Preston <luke.preston@bbc.co.uk>
2017-01-17 09:39:28 +0000

    tested removing a flow

--- src/web-router/routables/update/remove-flow.js
+++ src/web-router/routables/update/remove-flow.js
@@ -1,2 +1,12 @@
+import mapSenderFormats from '../common/map-sender-formats'
+
 export default ({data, grain}) => {
+  let flow = grain.pre
+  let index = -1
+  data.flows.forEach((f, i) => {
+    if (f.id === flow.id) index = i
+  })
+  data.flows.splice(index, 1)
+
+  mapSenderFormats(data)
 }
---
+++ src/web-router/routables/update/remove-flow.spec.js
@@ -0,0 +1,31 @@
+import remove from './remove-flow'
+import generate from '../../../ips-nmos-api/src/stub/generate'
+
+describe('adding flows', () => {
+  let data
+  beforeEach(() => {
+    let flow = generate.flow()
+    let sender = generate.sender()
+    sender.flow_id = flow.id
+    sender.format = 'faken'
+    flow.format = 'format'
+
+    data = {
+      senders: [sender],
+      flows: [flow]
+    }
+    let grain = {
+      post: {},
+      pre: flow
+    }
+    remove({data, grain})
+  })
+
+  it('removes flow to data', () => {
+    expect(data.flows.length).toBe(0)
+  })
+
+  it('updates senders with format', () => {
+    expect(data.senders[0].format).toBe('no')
+  })
+})
