1034ab48b610e6f1c0d514887bb8853eb4fc9237
Alexander Rawcliffe <alex.rawcliffe@bbc.co.uk>
2017-02-22 16:03:23 +0000

    Fix spaces in previous

--- src/init-nmos.js
+++ src/init-nmos.js
@@ -13,8 +13,8 @@ function getPrioritised (representations, queryPriority) {
         return representation.priority === queryPriority
       })[0]
     if (representation) {
-        return `http://${representation.address}:${representation.port}`
-      }
+      return `http://${representation.address}:${representation.port}`
+    }
   }
   let lessThanOneHundred = representations
     .filter(representation => {
