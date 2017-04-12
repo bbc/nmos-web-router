9fd0d086a594980a89262038cabb64b314f7628d
Andrew Bonney <andrew.bonney@bbc.co.uk>
2017-04-12 11:59:27 +0100

    choose: update all visible checkbox when new senders/receivers start

--- src/web-router/reducers/update-reducer.js
+++ src/web-router/reducers/update-reducer.js
@@ -1,12 +1,21 @@
 import Routables from '../routables'
+import allVisible from './choose/all-visible'
 
 export default (state, action, merge) => {
   let routables = Routables(state.view)
   let view = Object.assign({}, state.view, routables.view())
   if (action.name === 'view') return merge({view})
+
   routables.update[action.name](action.update[action.name])
   routables.filter(state.view.choose.term)
   let updatedView = routables.view()
   view = Object.assign({}, state.view, updatedView)
+
+  let allVisibleState = allVisible(routables.view().senders)
+  view.choose.allVisibleState.senders = allVisibleState.current
+
+  allVisibleState = allVisible(routables.view().receivers)
+  view.choose.allVisibleState.receivers = allVisibleState.current
+
   return merge({view})
 }
