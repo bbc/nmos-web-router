a7f6240e330ec7b09b71d4c7811e8b66d7d5cc92
Jason Loveridge <jason.loveridge@bbc.co.uk>
2017-06-22 12:12:45 +0100

    Confirm mode - Now uses most up-to-date sender info when routing (in confirm mode)

--- src/web-router/components/confirm/index.jsx
+++ src/web-router/components/confirm/index.jsx
@@ -13,7 +13,7 @@ import Header from './header-component'
 import ChangeQueue from './change-queue'
 import ClearConfirmButtons from './clear-confirm-buttons-component'
 
-let Confirm = ({changes, actions}) => {
+let Confirm = ({changes, actions, senders}) => {
   return <Layout className='confirm box box-hidden'>
     <Header />
     <ChangeQueue
@@ -21,13 +21,15 @@ let Confirm = ({changes, actions}) => {
       changes={changes} />
     <ClearConfirmButtons
       changes={changes}
-      actions={actions} />
+      actions={actions}
+      senders={senders} />
   </Layout>
 }
 
 Confirm.propTypes = {
   changes: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  senders: PropTypes.array.isRequired
 }
 
 export default Confirm
