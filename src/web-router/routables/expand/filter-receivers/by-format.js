ebe42f08a9866caf2b10a57ba44e8d0d32f0e535
Jason Loveridge <jasonl@ap-z200-1.rd.bbc.co.uk>
2017-08-08 09:44:07 +0100

    Receivers now checked individually for caps.media_types when filtering

--- src/web-router/routables/expand/filter-receivers/by-format.js
+++ src/web-router/routables/expand/filter-receivers/by-format.js
@@ -2,13 +2,11 @@ import mapState from '../../common/map-state'
 import stateToString from '../../common/state-to-string'
 import transportMatches from './transport-matches'
 
-export default (receivers, format, unicast) => {
-  receivers.forEach(receiver => {
-    let receiverMapState = mapState(receiver).notSelectable().enable()
-    if (receiver.format === format && transportMatches(receiver, unicast)) receiverMapState.selectable()
-    else if (format !== 'contracting') receiverMapState.disable()
+export default (receiver, format, unicast) => {
+  let receiverMapState = mapState(receiver).notSelectable().enable()
+  if (receiver.format === format && transportMatches(receiver, unicast)) receiverMapState.selectable()
+  else if (format !== 'contracting') receiverMapState.disable()
 
-    receiver.state = receiverMapState.state()
-    receiver.stateString = stateToString(receiver.state)
-  })
+  receiver.state = receiverMapState.state()
+  receiver.stateString = stateToString(receiver.state)
 }
--- src/web-router/routables/expand/filter-receivers/index.js
+++ src/web-router/routables/expand/filter-receivers/index.js
@@ -5,6 +5,10 @@ import byFormat from './by-format'
 export default (receivers, mediaType, format, unicast) => {
   // If expanded sender is unicast and is already routed then grey out all other receivers
   if (unicast.subscription_id) byUnicastSubscription(receivers, unicast)
-  else if (mediaType) byMediaType(receivers, mediaType, unicast)
-  else if (format) byFormat(receivers, format, unicast)
+  else {
+    receivers.forEach(receiver => {
+      if (receiver.caps.media_types && mediaType) byMediaType(receiver, mediaType, unicast)
+      else byFormat(receiver, format, unicast)
+    })
+  }
 }
