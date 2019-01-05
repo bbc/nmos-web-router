e9390f35ccb1f72abae823244cebb660cc9bb026
Danny Meloy <danny.meloy@bbc.co.uk>
2019-01-05 01:13:07 +0000

    get security url using mdns
    fixed get mDNS URL function
    
    adjusted service name and token endpoint

--- src/web-router/security/access-token.js
+++ src/web-router/security/access-token.js
@@ -5,13 +5,14 @@ export default (bearerToken) => {
     return true
   }
   let accessToken = ''
+  let storageName = 'bearerToken'
   if (window.sessionStorage) {
     if (bearerToken && validateToken(bearerToken)) {
-      window.sessionStorage.setItem('bearerToken', JSON.stringify(bearerToken))
+      window.sessionStorage.setItem(storageName, JSON.stringify(bearerToken))
       accessToken = jwtDecode(bearerToken.access_token)
       // If no parameters are passed, get token from session storage
-    } else if (!bearerToken && window.sessionStorage.getItem('bearerToken')) {
-      let bearerTokenString = JSON.parse(window.sessionStorage.getItem('bearerToken'))
+    } else if (!bearerToken && window.sessionStorage.getItem(storageName)) {
+      let bearerTokenString = JSON.parse(window.sessionStorage.getItem(storageName))
       accessToken = jwtDecode(bearerTokenString.access_token)
     }
   }
--- src/web-router/security/signin-request.js
+++ src/web-router/security/signin-request.js
@@ -1,24 +1,29 @@
 const axios = require('axios')
 const qs = require('qs')
+import { getServiceUrl } from '../../init-nmos'
 
 export default (username, password) => {
   let clientID = 'cdqgP5I6aMI3pvqacjkgroM7'
   let clientSecret = 'rectlNa4lGtC60BmFaE9gwKJQXcCnz0DDOxfUt7eRe7A0mby'
   let scope = 'is05'
-  let url = 'http://172.29.80.117:4999/oauth/token' // TODO discover URL via mDNS
   let data = {
     grant_type: 'password',
     scope: scope,
     username: username,
     password: password
   }
-  return axios({
-    url: url,
-    method: 'post',
-    data: qs.stringify(data),
-    auth: {
-      username: clientID,
-      password: clientSecret
-    }
-  })
+  // let url = 'http://172.29.80.117:4999/oauth/token' // TODO discover URL via mDNS
+
+  return getServiceUrl('nmos-auth', 'v1.0')
+    .then(href => {
+      return axios({
+        url: href + '/token',
+        method: 'post',
+        data: qs.stringify(data),
+        auth: {
+          username: clientID,
+          password: clientSecret
+        }
+      })
+    })
 }
