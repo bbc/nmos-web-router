d91cedcfb4a8fa4021d21b4c0bd237fad100ed7a
Pete Taylour <pete.taylour@gmail.com>
2019-01-30 12:47:59 +0000

    subscription: use const rather than var
    Co-Authored-By: andrewbonney <andrew@bonney.co.uk>

--- src/ips-nmos-api/src/http/subscription/create.js
+++ src/ips-nmos-api/src/http/subscription/create.js
@@ -6,7 +6,7 @@ module.exports = ({body, baseUrl, apiVersion, type, downgrade, downgradeVersion}
   if (downgrade) body.params = {'query.downgrade': `${downgradeVersion}`}
   else body.params = body.params || {}
   if (!body.hasOwnProperty('persist')) body.persist = false
-  var secure = baseUrl.startsWith('https')
+  const secure = baseUrl.startsWith('https')
   if (!body.hasOwnProperty('secure')) body.secure = secure
   body.resource_path = `/${type}`
   return axios.post(`${baseUrl}/${constants.QUERY_URL}/${apiVersion}/subscriptions`, body)
