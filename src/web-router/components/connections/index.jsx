279146ad217002f95586ac77df774ccd215e2d61
Richard Najdowski <richard.najdowski@bbc.co.uk>
2019-07-15 11:48:54 +0100

    Apply windowing and prevent new callback props with the `connections` component

--- src/web-router/components/connections/index.jsx
+++ src/web-router/components/connections/index.jsx
@@ -7,8 +7,16 @@ import Senders from './senders-component'
 import Routes from './routes-component'
 import Receivers from './receivers-component'
 import ChangeMode from './change-mode-component'
+import ScrollWindowing from '../scroll-windowing/scroll-windowing'
+import { ROUTABLE_ROW_HEIGHT } from '../scroll-windowing/constants'
+
+const filterByUnchecked = ({ stateString }) => !stateString.includes('unchecked')
 
 let Connections = ({expanded, senders, receivers, routes, actions, routingMode, changes, location, choose}) => {
+  // Filter the senders and receivers
+  const filteredSenders = senders.filter(filterByUnchecked)
+  const filteredReceivers = receivers.filter(filterByUnchecked)
+
   return <Layout className='connections box box-hidden connections-box'
     onClick={function () { if (!expanded.state.includes('contracted')) actions.toggleSender(expanded) }}>
     <ChangeMode
@@ -18,24 +26,40 @@ let Connections = ({expanded, senders, receivers, routes, actions, routingMode, 
       choose={choose}
       actions={actions} />
     <Header />
-    <Layout gels='1/1' layouts='flush' className='routables-scroll' onScroll={() => { if (expanded.state.length > 0 && !expanded.state.includes('contracted')) { actions.update() } }}>
-      <Senders
-        senders={senders}
-        actions={actions}
-        />
-      <Receivers
-        senders={senders}
-        receivers={receivers}
-        actions={actions}
-        routingMode={routingMode}
-        changes={changes}
-          />
-      <Routes
-        routes={routes}
-        actions={actions}
-        expanded={expanded}
-        />
-    </Layout>
+    <ScrollWindowing rowHeight={ROUTABLE_ROW_HEIGHT}>
+      {({ getScrollProps, visibleStartRow, visibleEndRow }) => (
+        <div className='routables-scroll' {...getScrollProps({
+          onScroll: () => {
+            if (expanded.state.length > 0 && !expanded.state.includes('contracted')) {
+              actions.update()
+            }
+          }
+        })}>
+          <Layout gels='1/1' layouts='flush'>
+            <Senders
+              senders={filteredSenders}
+              actions={actions}
+              visibleStartRow={visibleStartRow}
+              visibleEndRow={visibleEndRow}
+              />
+            <Receivers
+              senders={senders}
+              receivers={filteredReceivers}
+              actions={actions}
+              routingMode={routingMode}
+              changes={changes}
+              visibleStartRow={visibleStartRow}
+              visibleEndRow={visibleEndRow}
+              />
+            <Routes
+              routes={routes}
+              actions={actions}
+              expanded={expanded}
+              />
+          </Layout>
+        </div>
+      )}
+    </ScrollWindowing>
   </Layout>
 }
 
